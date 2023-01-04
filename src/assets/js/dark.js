4784cb5eebd7f2d319ae5bb4d0ffc43dd846fa40
Mike Neal <mike@miken.dev>
2023-01-04 18:53:17 -0500

    Initial commit

---
+++ src/assets/js/dark.js
@@ -0,0 +1,50 @@
+
+
+//
+//    The Dark Mode System
+//
+
+/* Body and Toggle */
+var body = document.querySelector("body");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+// Get Status of Dark Mode
+let darkMode = localStorage.getItem("darkMode");
+
+// Enable Dark Mode
+const enableDarkMode = () => {
+  body.classList.add("dark-mode");
+  localStorage.setItem("darkMode", "enabled")
+}
+
+// Disable Dark mode
+const disableDarkMode = () => {
+  body.classList.remove("dark-mode");
+  localStorage.setItem("darkMode", null)
+}
+
+// Check the state of dark mode in local storage
+if (darkMode == "enabled") {
+  enableDarkMode();
+}
+//check user's OS level dark mode preference
+const darkModePreference = window.matchMedia('(prefers-color-scheme: dark)');
+
+// Check if the user has a preference for dark mode
+if (darkModePreference.matches) {
+  // If the user has a preference for dark mode, enable it
+  enableDarkMode();
+}
+
+// add event listener to the dark mode button toggle
+darkModeToggle.addEventListener('click', () => {
+  // on click, check localstorage for the dark mode value
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    // if dark mode is not enabled, run this function to set it to enabled
+    enableDarkMode();
+  } else {
+    // if dark mode is enabled, run this function to set it to disabled
+    disableDarkMode();
+  }
+})
